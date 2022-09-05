c484e43cc5e09b6f9a970951ba4bec8426287074
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-09-05 14:45:31 +0530

    mongoose connection and admn module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AdminModule } from './admin/admin.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://macsof:macsof@nextlevelcarwash.yjs3i.mongodb.net/owbc?retryWrites=true&w=majority',
+    ),
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
