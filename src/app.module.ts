be566104962a85beef79211b7cb9d87fbed856a3
Manikanta <kantipudimanikanta2000@gmail.com>
2022-09-05 15:23:33 +0530

    user module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://macsof:macsof@nextlevelcarwash.yjs3i.mongodb.net/owbc?retryWrites=true&w=majority',
     ),
     AdminModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
